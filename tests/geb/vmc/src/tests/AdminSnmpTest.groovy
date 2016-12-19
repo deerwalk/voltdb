0825ad14c6404249b1e71894d8a24b184579a696
anil <anrai@deerwalk.com>
2016-12-19 10:03:31 +0545

    Initial geb tests for snmp

---
+++ tests/geb/vmc/src/tests/AdminSnmpTest.groovy
@@ -0,0 +1,86 @@
+/* This file is part of VoltDB.
+ * Copyright (C) 2008-2016 VoltDB Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+ /**
+ * Created by anrai on 2/12/15.
+ */
+
+package vmcTest.tests
+
+import org.junit.Test
+import vmcTest.pages.*
+import geb.Page.*
+
+/**
+ * This class contains tests of the 'Admin' tab of the VoltDB Management
+ * Center (VMC) page, which is the VoltDB (new) web UI.
+ */
+
+class AdminSnmpTest extends TestBase {
+    def setup() { // called before each test
+        count = 0
+
+        while(count<numberOfTrials) {
+            count ++
+            try {
+                setup: 'Open VMC page'
+                to VoltDBManagementCenterPage
+                page.loginIfNeeded()
+                expect: 'to be on VMC page'
+                at VoltDBManagementCenterPage
+
+                when: 'click the Admin link (if needed)'
+                page.openAdminPage()
+                then: 'should be on Admin page'
+                at AdminPage
+
+                break
+            } catch (org.openqa.selenium.ElementNotVisibleException e) {
+                println("ElementNotVisibleException: Unable to Start the test")
+                println("Retrying")
+            }
+        }
+    }
+
+    def checkSnmpTitles() {
+        when: 'check snmp title'
+        page.snmpTitle.isDisplayed()
+        and: 'click snmp title'
+        page.snmpTitle.click()
+        then: 'check titles under snmp'
+        page.snmpTarget.isDisplayed()
+        page.snmpCommunity.isDisplayed()
+        page.snmpUsername.isDisplayed()
+        page.snmpAuthenticationProtocol.isDisplayed()
+        page.snmpAuthenticationKey.isDisplayed()
+        page.snmpPrivacyProtocol.isDisplayed()
+        page.snmpPrivacyKey.isDisplayed()
+    }
+
+    def checkSnmpButtons() {
+        when: 'check snmp edit button'
+        page.snmpEditButton.isDisplayed()
+        then: 'click snmp button'
+        page.snmpEditButton.click()
+    }
+}
